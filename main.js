a323d718e0a9763de5d9fc8a51db594e817d5038
uplusion23 <uplusion23@gmail.com>
2021-03-31 21:21:25 -0500

    Update Styling, Fix Resizing
    Style Update, smaller CSS file.
    Fixed the window resizing.
    Output shows timestamps now.

--- main.js
+++ main.js
@@ -5,13 +5,16 @@ const {app, BrowserWindow} = require("electron");
 app.whenReady().then(function() {
     const win = new BrowserWindow({
         width: 600,
+        minWidth: 600,
         height: 790,
+        minHeight: 790,
         webPreferences: {
             nodeIntegration: true
         }
     });
 
     win.removeMenu();
+    win.webContents.openDevTools();
     win.loadFile("index.html");
 });
 
